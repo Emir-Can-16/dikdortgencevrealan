47021cec2b006eb36aebca8758a7f67c2449f5a5
Emircan Türkmen <221903523@ogr.uludag.edu.tr>
2020-11-30 14:45:24 +0300

    Proje dosyası ekle.

---
+++ dikdörgencevrealan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dikdörgencevrealan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ukenar = 0, kkenar = 0;
+            Console.Write("Diktörgenin uzun kenarını giriniz:");
+            ukenar = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Diktörgenin kısa kenarını giriniz:");
+            kkenar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Çevre= {0}", ((ukenar + kkenar) * 2).ToString());
+            Console.WriteLine("Alan= {0}", (ukenar * kkenar).ToString());
+            Console.ReadKey();
+        }
+    }
+}
